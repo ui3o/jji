fc43ce0d522aebc5ea2dbae2e44b67eab967b01a
ui3o.com <ui3o.com@gmail.com>
2021-10-05 12:12:44 +0200

    double lazy load example

--- example/a.lazy.3.jj.js
+++ example/a.lazy.3.jj.js
@@ -3,6 +3,14 @@ module.exports.as_all = $('later load desc', (res) => {
         res({
             test_run: "echo lazy test",
             test_all_run: () => { console.log('simple eval'); _`sleep 3 && echo hello world`; },
+            test_later: $('later load desc', (res) => {
+                setTimeout(() => {
+                    res({
+                        test_run: "echo lazy test",
+                        test_all_run: () => { console.log('simple eval'); _`sleep 3 && echo hello world`; },
+                    })
+                }, 5000);
+            })
         })
     }, 5000);
 })
