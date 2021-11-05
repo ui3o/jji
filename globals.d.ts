3e8408b182bcd523bd5bf7af328c9919fedda849
ui3o.com <ui3o.com@gmail.com>
2021-11-05 12:43:13 +0100

    code helper fix

--- globals.d.ts
+++ globals.d.ts
@@ -132,9 +132,81 @@ declare interface IJJ {
    *
    */
   cli: ICLI;
+  term: {
+    /**
+     * modifier codes
+     */
+    mc: {
+      clearLine: `\x1b[2K`;
+      styleReset: "\x1b[0m";
+      bold: "\x1b[1m";
+      italic: "\x1b[3m";
+      underline: "\x1b[4m";
+      inverse: "\x1b[7m";
+      strike: "\x1b[9m";
+    };
+    /**
+     * foreground colors
+     */
+    fc: {
+      defaultColor: "\x1b[39m";
+      black: "\x1b[30m";
+      red: "\x1b[31m";
+      green: "\x1b[32m";
+      yellow: "\x1b[33m";
+      blue: "\x1b[34m";
+      magenta: "\x1b[35m";
+      cyan: "\x1b[36m";
+      white: "\x1b[37m";
+      brightBlack: "\x1b[90m";
+      brightRed: "\x1b[91m";
+      brightGreen: "\x1b[92m";
+      brightYellow: "\x1b[93m";
+      brightBlue: "\x1b[94m";
+      brightMagenta: "\x1b[95m";
+      brightCyan: "\x1b[96m";
+      brightWhite: "\x1b[97m";
+      customColor(code): string;
+    };
+    /**
+     * background colors
+     */
+    bc: {
+      defaultColor: "\x1b[49m";
+      black: "\x1b[40m";
+      red: "\x1b[41m";
+      green: "\x1b[42m";
+      yellow: "\x1b[43m";
+      blue: "\x1b[44m";
+      magenta: "\x1b[45m";
+      cyan: "\x1b[46m";
+      white: "\x1b[47m";
+      brightBlack: "\x1b[100m";
+      brightRed: "\x1b[101m";
+      brightGreen: "\x1b[102m";
+      brightYellow: "\x1b[103m";
+      brightBlue: "\x1b[104m";
+      brightMagenta: "\x1b[105m";
+      brightCyan: "\x1b[106m";
+      brightWhite: "\x1b[107m";
+      customBgColor(code): string;
+    };
+  };
 }
 
 declare interface ICL {
+  /**
+   * **wd** means set the process working directory
+   * @param wd path to working directory
+   */
+  wd(wd: string): ICL;
+  /**
+   * **do** means spawn the command
+   */
+  do(commandLine: string);
+}
+
+declare interface ICLI extends ICL {
   /**
    * **hideErr** means the error output will not printed to stdout
    */
@@ -144,24 +216,12 @@ declare interface ICL {
    */
   noErr: ICL;
   /**
-   * **wd** means set the process working directory
-   * @param wd path to working directory
-   */
-  wd(wd: string): ICL;
-  /**
    * **eol** set end of line characters
    *
    * **default**: '\n'
    * @param eol end of line characters
    */
   eol(eol: string): ICL;
-  /**
-   * **do** means spawn the command
-   */
-  do(commandLine: string);
-}
-
-declare interface ICLI extends ICL {
   /**
    * **splitByLine** means the output will be split by line
    */
